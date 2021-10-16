0194c58673994a45fcc69e923fa3d6b987b1a499
Adhip-choudhury <79558085+Adhip-choudhury@users.noreply.github.com>
2021-10-16 09:47:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,342 @@
+
+
+
+
+function setup(){
+    canvas= createCanvas(640, 480);
+    canvas.position(150, 150);
+    video= createCapture(VIDEO);
+    video.hide();
+}
+    
+function draw(){
+    image(video, 0, 0, 640 ,480);
+
+// it was a shade of red
+    fill(128, 0, 0);
+    stroke(128, 0, 0);
+    circle(50, 50, 80);
+
+
+    fill(153, 0, 0);
+    stroke(153, 0, 0);
+    circle(70, 50, 80);
+
+
+
+    
+    fill(179, 0, 0);
+    stroke(179, 0, 0);
+    circle(90, 50, 80);
+
+    
+    fill(204, 0, 0);
+    stroke(204, 0, 0);
+    circle(110, 50, 80);
+
+
+    
+    fill(230, 0, 0);
+    stroke(230, 0, 0);
+    circle(130, 50, 80);
+
+    
+    fill(255, 0, 0);
+    stroke(255, 0, 0);
+    circle(150, 50, 80);
+
+
+    // it was a shade of orange
+
+
+    fill(128, 26, 0);
+    stroke(128, 26, 0);
+    circle(170, 50, 80);
+
+
+    fill(153, 31, 0);
+    stroke(153, 31, 0);
+    circle(190, 50, 80);
+
+
+
+    
+    fill(179, 36, 0);
+    stroke(179, 36, 0);
+    circle(210, 50, 80);
+
+    
+    fill(204, 41, 0);
+    stroke(204, 41, 0);
+    circle(230, 50, 80);
+
+
+    
+    fill(230, 46, 0);
+    stroke(230, 46, 0);
+    circle(250, 50, 80);
+
+    
+    fill(255, 51, 0);
+    stroke(255, 51, 0);
+    circle(270, 50, 80);
+
+
+    // it was the shade of yellow
+
+    fill(128, 51, 0);
+    stroke(128, 51, 0);
+    circle(290, 50, 80);
+
+
+    fill(153, 61, 0);
+    stroke(153, 61, 0);
+    circle(310, 50, 80);
+
+
+
+    
+    fill(179, 71, 0);
+    stroke(179, 71, 0);
+    circle(330, 50, 80);
+
+    
+    fill(204, 82, 0);
+    stroke(204, 82, 0);
+    circle(350, 50, 80);
+
+
+    
+    fill(179, 71, 0);
+    stroke(204, 82, 0);
+    circle(370, 50, 80);
+
+    
+    fill(255, 102, 0);
+    stroke(255, 102, 0);
+    circle(390, 50, 80);
+
+        // it was the shade of yellow
+
+        fill(128, 64, 0);
+        stroke(128, 64, 0);
+        circle(410, 50, 80);
+    
+    
+        fill(153, 77, 0);
+        stroke(153, 77, 0);
+        circle(430, 50, 80);
+    
+    
+    
+        
+        fill(179, 89, 0);
+        stroke(179, 89, 0);
+        circle(450, 50, 80);
+    
+        
+        fill(204, 102, 0);
+        stroke(204, 102, 0);
+        circle(470, 50, 80);
+    
+    
+        
+        fill(230, 115, 0);
+        stroke(230, 115, 0);
+        circle(490, 50, 80);
+    
+        
+        fill(255, 128, 0);
+        stroke(255, 128, 0);
+        circle(510, 50, 80);
+
+
+
+
+        fill(255, 117, 26);
+        stroke(255, 117, 26);
+        circle(530, 50, 80);
+    
+        
+        fill(255, 133, 51);
+        stroke(255, 133, 51);
+        circle(550, 50, 80);
+
+               
+        fill(255, 148, 77);
+        stroke(255, 148, 77);
+        circle(570, 50, 80);
+
+        fill(255, 163, 102);
+        stroke(255, 163, 102);
+        circle(590, 50, 80);
+
+
+
+
+
+
+
+
+
+
+
+        // it was a shade of red
+    fill(128, 0, 0);
+    stroke(128, 0, 0);
+    circle(50, 430, 80);
+
+
+    fill(153, 0, 0);
+    stroke(153, 0, 0);
+    circle(70, 430, 80);
+
+
+
+    
+    fill(179, 0, 0);
+    stroke(179, 0, 0);
+    circle(90, 430, 80);
+
+    
+    fill(204, 0, 0);
+    stroke(204, 0, 0);
+    circle(110, 430, 80);
+
+
+    
+    fill(230, 0, 0);
+    stroke(230, 0, 0);
+    circle(130, 430, 80);
+
+    
+    fill(255, 0, 0);
+    stroke(255, 0, 0);
+    circle(150, 430, 80);
+
+
+    // it was a shade of orange
+
+
+    fill(128, 26, 0);
+    stroke(128, 26, 0);
+    circle(170, 430, 80);
+
+
+    fill(153, 31, 0);
+    stroke(153, 31, 0);
+    circle(190, 430, 80);
+
+
+
+    
+    fill(179, 36, 0);
+    stroke(179, 36, 0);
+    circle(210, 430, 80);
+
+    
+    fill(204, 41, 0);
+    stroke(204, 41, 0);
+    circle(230, 430, 80);
+
+
+    
+    fill(230, 46, 0);
+    stroke(230, 46, 0);
+    circle(250, 430, 80);
+
+    
+    fill(255, 51, 0);
+    stroke(255, 51, 0);
+    circle(270, 430, 80);
+
+
+    // it was the shade of yellow
+
+    fill(128, 51, 0);
+    stroke(128, 51, 0);
+    circle(290, 430, 80);
+
+
+    fill(153, 61, 0);
+    stroke(153, 61, 0);
+    circle(310, 430, 80);
+
+
+
+    
+    fill(179, 71, 0);
+    stroke(179, 71, 0);
+    circle(330, 430, 80);
+
+    
+    fill(204, 82, 0);
+    stroke(204, 82, 0);
+    circle(350, 430, 80);
+
+
+    
+    fill(179, 71, 0);
+    stroke(204, 82, 0);
+    circle(370, 430, 80);
+
+    
+    fill(255, 102, 0);
+    stroke(255, 102, 0);
+    circle(390, 430, 80);
+
+        // it was the shade of yellow
+
+        fill(128, 64, 0);
+        stroke(128, 64, 0);
+        circle(410, 430, 80);
+    
+    
+        fill(153, 77, 0);
+        stroke(153, 77, 0);
+        circle(430, 430, 80);
+    
+    
+    
+        
+        fill(179, 89, 0);
+        stroke(179, 89, 0);
+        circle(450, 430, 80);
+    
+        
+        fill(204, 102, 0);
+        stroke(204, 102, 0);
+        circle(470, 430, 80);
+    
+    
+        
+        fill(230, 115, 0);
+        stroke(230, 115, 0);
+        circle(490, 430, 80);
+    
+        
+        fill(255, 128, 0);
+        stroke(255, 128, 0);
+        circle(510, 430, 80);
+
+
+
+
+        fill(255, 117, 26);
+        stroke(255, 117, 26);
+        circle(530, 430, 80);
+    
+        
+        fill(255, 133, 51);
+        stroke(255, 133, 51);
+        circle(550, 430, 80);
+
+               
+        fill(255, 148, 77);
+        stroke(255, 148, 77);
+        circle(570, 430, 80);
+
+        fill(255, 163, 102);
+        stroke(255, 163, 102);
+        circle(590, 430, 80);
+}
